94d9f2359f4123018d2273fe056602a962f374f5
Duke <dukethanakorn@gmail.com>
2024-12-28 09:55:22 +0000

    Sat, Dec 28, 2024, 4:55 PM +07:00

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -6,10 +6,11 @@ const SearchBox = ({searchfield, searchChange}) => {
                 <input
                     className = 'pa3 br4 ba b--blue bg-white-90 dark-blue w-50-ns w-80
                           hover-bg-white transition-colors input-reset
-                          shadow-4 tracked placeholder-dark-blue'
+                          shadow-4 tracked placeholder-dark-blue fw7'
                     type='search'
                     placeholder='search robots'
                     onChange = {searchChange}
+                    style={{ fontWeight: 'bold' }}
                 />
         </div>
     );
