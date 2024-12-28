475ddf9890d693811f8bfe8846a7f2c0dd5ad89d
Duke <dukethanakorn@gmail.com>
2024-12-28 04:55:11 +0000

    Sat, Dec 28, 2024, 11:55 AM +07:00

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import CardList from './CardList';
+import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import {robots} from './robots';
+
+// Create the root container for your app
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the Card component
+root.render(
+  <CardList robots={robots} />
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
