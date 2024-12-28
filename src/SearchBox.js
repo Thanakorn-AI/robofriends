1da311ad7ca6f3b0207a0b34368921a88bacdbe4
Duke <dukethanakorn@gmail.com>
2024-12-28 09:09:08 +0000

    Sat, Dec 28, 2024, 4:09 PM +07:00

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const SearchBox = ({searchfield, searchChange}) => {
     return (
-        <div className='pa2'>
+        <div className='pa3 mb4'>
                 <input
-                    className = 'pa3 ba b--green bg-lightest-blue'
+                    className = 'pa3 br3 ba b--light-red bg-dark-gray white-90 w-50-ns w-80
+                          hover-bg-mid-gray transition-colors input-reset outline-0
+                          shadow-5 tracked'
                     type='search'
                     placeholder='search robots'
                     onChange = {searchChange}
